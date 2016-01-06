9f62b6e788261ccfd7d493b510b8a0f697ce72b5
roberttmoon@gmail.com <roberttmoon@gmail.com>
2016-01-06 15:23:03 -0600

    new bad way

---
+++ Calculator/MathClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class MathClass
+    {
+        public string Multiply(string incExpression)
+        {
+            string[] multiStrings = incExpression.Split('*');
+            float multiReslut = float.Parse(multiStrings[0]) * float.Parse(multiStrings[1]);
+            string reslutNumber = multiReslut.ToString();
+            return reslutNumber;
+        }
+        public string Divide(string incExpression)
+        {
+            string[] divString = incExpression.Split('/');
+            if (divString[1] == "0")
+                return "DivBy0";
+            else
+            {
+                float divReslut = float.Parse(divString[0]) / float.Parse(divString[1]);
+                string reslutNumber = divReslut.ToString();
+                return reslutNumber;
+            }
+        }
+        public string Add(string incExpression)
+        {
+            string[] addString = incExpression.Split('+');
+            float addReslut = float.Parse(addString[0]) + float.Parse(addString[1]);
+            string reslutNumber = addReslut.ToString();
+            return reslutNumber;
+        }
+        public string Subtract(string incExpression)
+        {
+            string[] subtractString = incExpression.Split('-');
+            float subReslut = float.Parse(subtractString[0]) - float.Parse(subtractString[1]);
+            string reslutNumber = subReslut.ToString();
+            return reslutNumber;
+        }
+    }
+}
--- Calculator/StringParcer.cs
+++ Calculator/StringParcer.cs
@@ -2,16 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class StringParcer
+    public class StringParcer : IDoOperations
     {
-
-        public void findNumber()
+        public void checkForNumber(string incomingExpression)
         {
-            
+           // if(
+        }
+        public string DoOperations(string incomingExpression)
+        {
+            if (incomingExpression.Contains("*"))
+            {
+                string multiOut;
+                string multiReturn;
+                MathClass mathClass = new MathClass();
+                multiOut = Regex.Match(incomingExpression, @"[0-9]*['\*'][0-9]*").ToString();
+                multiReturn = mathClass.Multiply(multiOut);
+                incomingExpression = Regex.Replace(incomingExpression, @"[0-9]*['\*'][0-9]*", multiReturn).ToString();
+                return incomingExpression;
+            }
+            else if (incomingExpression.Contains("/"))
+            {
+                string divOut, divReturn;
+                MathClass mathClass = new MathClass();
+                divOut = Regex.Match(incomingExpression, @"[0-9]*['\*'][0-9]*").ToString();
+                divReturn = mathClass.Subtract(divOut);
+                incomingExpression = Regex.Replace(incomingExpression, @"[0-9]*['\/'][0-9]*", divReturn).ToString();
+                return incomingExpression;
+            }
+            else if (incomingExpression.Contains("+"))
+            {
+                string addOut, addReturn;
+                MathClass mathClass = new MathClass();
+                addOut = Regex.Match(incomingExpression, @"[0-9]*['\+'][0-9]*").ToString();
+                addReturn = mathClass.Add(addOut);
+                incomingExpression = Regex.Replace(incomingExpression, @"[0-9]*['\+'][0-9]*", addReturn).ToString();
+                return incomingExpression;
+            }
+            else if (incomingExpression.Contains("-"))
+            {
+                string subOut, subReturn;
+                MathClass mathClass = new MathClass();
+                subOut = Regex.Match(incomingExpression, @"[0-9]*['\+'][0-9]*").ToString();
+                subReturn = mathClass.Subtract(subOut);
+                incomingExpression = Regex.Replace(incomingExpression, @"[0-9]*['\*'][0-9]*", subReturn).ToString();
+                return incomingExpression;
+            }
+            else
+                throw new NotImplementedException("invalid input");
         }
     }
 }
